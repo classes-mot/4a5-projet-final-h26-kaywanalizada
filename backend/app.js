68f67a91c1779253cd14d8fe723cddb5eb296e05
Kaywan Alizada <2497883@cmontmorency.qc.ca>
2026-05-04 16:22:12 -0400

    FAIT: Début déploiement

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,8 @@ app.use('/api/users', routeUser);
 app.use('/api/quiz', routeQuiz);
 
 await connectDB();
-const port = 5000;
+const port = process.env.PORT ||5000;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://kaywanali2007_db_user:kaywanali2007_db_password@clusterprojetfinalwebet.9ku4z8v.mongodb.net/?appName=ClusterProjetFinalWebEtBase";
 app.listen(port, () => {
     console.log(`Serveur démarré sur le port ${port}`);
 })
