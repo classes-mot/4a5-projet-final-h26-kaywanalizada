51b3849a4ca4fcce2af6a07360aacb0cfd968394
Kaywan Alizada <2497883@cmontmorency.qc.ca>
2026-05-05 08:52:05 -0400

    FAIT:affichage de base de la page Jouer.jsx

--- frontend/src/Pages/Jouer.jsx
+++ frontend/src/Pages/Jouer.jsx
@@ -1,3 +1,22 @@
+import { useParams, useNavigate } from "react-router-dom";
 export default function Jouer() {
-    
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const quiz = JSON.parse(localStorage.getItem("quiz")).find((q) => q.id === id)
+    const questions = quiz ? quiz.questions : [];
+    return (
+        <div>
+            <h2>{quiz.title}</h2>
+           {questions.map((q, index) => (
+             <div key={index}>
+                <p>Question {index+ 1} : {q.question}</p>
+                <input 
+                type="text"
+                placeholder="Votre réponse"/>
+             </div>
+           ))}
+
+           <button>Soumettre</button>
+        </div>
+    )
 }
