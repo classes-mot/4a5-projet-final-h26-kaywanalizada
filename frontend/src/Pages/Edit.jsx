ec4ce9cdd3063d42def6e6295b8fe027f01902a8
Kaywan Alizada <2497883@cmontmorency.qc.ca>
2026-05-04 20:13:25 -0400

    FAIT: Ajout du question/reponses quand on update un quiz

--- frontend/src/Pages/Edit.jsx
+++ frontend/src/Pages/Edit.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function UpdateTask() {
   const { id } = useParams();
@@ -8,6 +9,22 @@ export default function UpdateTask() {
   const storedQuiz = JSON.parse(localStorage.getItem("quiz")) || [];
 
   const quizSelected = storedQuiz.find((g) => g.id === id);
+
+  const [questions, setQuestions] = useState(quizSelected?.questions || []);
+
+      const ajouterQuestion = () => {
+       setQuestions([...questions, { id:Math.random().toString(), question: "", reponse: ""}]);
+    }
+
+    const modifierQuestion = (index, champ, valeur) => {
+       const nouvelleQuestions = [...questions];
+       nouvelleQuestions[index][champ] = valeur;
+      setQuestions(nouvelleQuestions);
+    }
+
+    const supprimerQuestion = (index) => {
+        setQuestions(questions.filter((q, i) => i !== index));
+    }
 
   if (!quizSelected) {
     return (
@@ -29,7 +46,8 @@ export default function UpdateTask() {
             ...g,
             title: data.title,
             type: data.type,
-            nbQuestions: data.nbQuestions,
+            nbQuestions: questions.length,
+            questions: questions
           }
         : g,
     );
@@ -72,6 +90,40 @@ export default function UpdateTask() {
           type="number"
           name="nbQuestions"
         />
+      </div>
+      <div className="control">
+        <label htmlFor="questions">Questions</label>
+        {questions.map((q, index) => (
+          <div key={q.id} className="block-question">
+            <div className="control">
+            <label>Question {index + 1}</label>
+            <input
+              type="text"
+              value={q.question}
+              onChange = {(e) => modifierQuestion(index, "question", e.target.value)}
+              required
+              />
+              </div>
+
+            <div className="control">
+            <label>Bonne réponse</label>
+            <input
+              type="text"
+              value={q.reponse}
+              onChange = {(e) => modifierQuestion(index, "reponse", e.target.value)}
+              required
+              />
+              </div>
+
+            <button type="button" onClick={() => supprimerQuestion(index)}>
+            Supprimer question
+          </button>
+          </div>
+        ))}
+        <button type="button" onClick={ajouterQuestion}>
+          Ajouter Question
+        </button>
+
       </div>
 
       <p className="form-actions">
