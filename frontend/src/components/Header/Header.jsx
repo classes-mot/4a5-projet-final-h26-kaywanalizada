ac68c1cd68ed64183577c9c83ea07811aa5e9a59
Kaywan Alizada <2497883@cmontmorency.qc.ca>
2026-05-06 15:30:23 -0400

    FAIT: traduction Header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,29 +3,31 @@ import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import "./Header.css";
 import LanguageSwitcher from "../LanguageSwitcher/LanguageSwitcher";
+import { useTranslation } from "react-i18next";
 
 export default function Header() {
   const { isLoggedIn, logout } = useAuth();
   const navigate = useNavigate();
+  const { t } = useTranslation();
   const bonLogout = () => {
     logout();
     navigate("/quizList");
   };
   return (
     <header className="main-header">
-      <h1 className="title">Quiz de Football</h1>
+      <h1 className="title">{t("Quiz de Football")}</h1>
 
       <nav className="nav-links">
-        <Link to="/QuizList">Mes quiz</Link>
-        {isLoggedIn && <Link to="/add">Ajouter un quiz</Link>}
+        <Link to="/QuizList">{t("Mes quiz")}</Link>
+        {isLoggedIn && <Link to="/add">{t("Ajouter un quiz")}</Link>}
         {isLoggedIn ? (
           <button onClick={bonLogout} className="logout-btn">
-            Se déconnecter
+            {t("Se déconnecter")}
           </button>
         ) : (
           <>
-          <Link to="/login">Se connecter</Link>
-          <Link to="/signup">Créer un compte</Link>
+          <Link to="/login">{t("Se connecter")}</Link>
+          <Link to="/signup">{t("Créer un compte")}</Link>
           </>
         )}
         <LanguageSwitcher />
