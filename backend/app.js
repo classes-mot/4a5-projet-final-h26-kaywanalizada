11cd5ecc56fed59e0597976b1bb801093594b645
Kaywan Alizada <2497883@cmontmorency.qc.ca>
2026-05-06 11:14:20 -0400

    FAIT:utilisation cors dans app.js du backend

--- backend/app.js
+++ backend/app.js
@@ -3,18 +3,18 @@ import { connectDB } from './util/bs.js';
 import checkAuth from './middleware/check-auth.js';
 import routeQuiz from './routes/quiz-route.js';
 import routeUser from './routes/users-route.js';
+import cors from 'cors';
 await connectDB();
 const app = express();
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers',
-        'Origin, X-requested-With, Content-Type, Accept, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-    next();
-});
+app.use(cors({
+origin: '*',
+methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 
 app.use('/api/users', routeUser);
 
